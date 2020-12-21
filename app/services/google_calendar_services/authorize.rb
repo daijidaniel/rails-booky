990b962f8d1017f74856af8489cb71a13cd788c1
archetype2142 <malhotraritwick2011@gmail.com>
2020-12-21 20:44:36 +0100

    fix booking relation

--- app/services/google_calendar_services/authorize.rb
+++ app/services/google_calendar_services/authorize.rb
@@ -5,22 +5,20 @@ module GoogleCalendarServices
   class Authorize
     class << self
       def execute!
-        client_id = Google::Auth::ClientId.from_file CREDENTIALS_PATH
-        token_store = Google::Auth::Stores::FileTokenStore.new file: TOKEN_PATH
-        authorizer = Google::Auth::UserAuthorizer.new client_id, SCOPE, token_store
-        user_id = "default"
-        credentials = authorizer.get_credentials user_id
-        if credentials.nil?
-          url = authorizer.get_authorization_url base_url: OOB_URI
-          puts "Open the following URL in the browser and enter the " \
-               "resulting code after authorization:\n" + url
-          code = gets
-          credentials = authorizer.get_and_store_credentials_from_code(
-            user_id: user_id, code: code, base_url: OOB_URI
-          )
-        end
-        credentials
+
+        scopes =  [
+          'https://www.googleapis.com/auth/calendar'    
+        ]
+
+        
+        authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
+          json_key_io: File.open('./app/services/google_calendar_services/creds.json'),
+          scope: scopes
+        )
+
+        authorizer.fetch_access_token!
       end
     end
   end
 end
+
