3d0e6b786bbb0623997c3ba0775d96195a06953e
archetype2142 <malhotraritwick2011@gmail.com>
2020-12-22 03:48:10 +0100

    asd

--- app/controllers/order_completed_controller.rb
+++ app/controllers/order_completed_controller.rb
@@ -2,8 +2,12 @@ class OrderCompletedController < ApplicationController
   def index
     @payment = Payment.find_by(id: params[:payment])
     
-    unless current_user.payments.find_by(id: @payment.id)
-      @payment = nil 
+    if user_signed_in?
+      unless current_user.payments.find_by(id: @payment.id)
+        @payment = nil 
+      end
+    else
+      redirect_to new_user_session_path, flash: { error: "Must be signed in" }
     end
   end
 end
