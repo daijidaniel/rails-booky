5ec1b2263dabdb778e3be799562bc578c5dbce12
archetype2142 <malhotraritwick2011@gmail.com>
2021-01-07 12:34:34 +0100

    mailer bug fix

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,19 +2,22 @@
 class UserMailerPreview < ActionMailer::Preview
   def order_mail_placed
     UserMailer.order_mail_placed(
-      Payment.last.id
+      Payment.last.id,
+      "test@test.com"
     )
   end
 
   def order_mail_paid
     UserMailer.order_mail_placed(
-      Payment.last.id
+      Payment.last.id,
+      "test@test.com"
     )
   end
 
   def admin_mail
     UserMailer.order_mail_placed(
-      Payment.last.id
+      Payment.last.id,
+      "test@test.com"
     )
   end
 end
